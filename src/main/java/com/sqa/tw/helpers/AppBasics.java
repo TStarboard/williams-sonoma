39cf264e9aff43ea41e84025d511da3c6a0d8671
Tony Wong <tswong@gmail.com>
2017-05-03 19:22:21 -0700

    Update AppBasics

--- src/main/java/com/sqa/tw/helpers/AppBasics.java
+++ src/main/java/com/sqa/tw/helpers/AppBasics.java
@@ -16,10 +16,51 @@ public class AppBasics {
 		return scanner.nextLine();
 	}
 
+	public static boolean requestBoolean(String question) {
+		System.out.print(question + " (true/false): ");
+		String input = scanner.nextLine();
+		boolean value = Boolean.parseBoolean(input);
+		return value;
+	}
+
+	public static char requestChar(String question) {
+		System.out.print(question + " ");
+		String input = scanner.nextLine();
+		char character = input.charAt(0);
+		return character;
+	}
+
+	public static float requestFloat(String question) {
+		System.out.print(question + " ");
+		String input = scanner.nextLine();
+		float value = Float.parseFloat(input);
+		return value;
+	}
+
 	public static int requestInt(String question) {
 		System.out.print(question + " ");
 		String input = scanner.nextLine();
 		int value = Integer.parseInt(input);
 		return value;
+	}
+
+	public static long requestLong(String question) {
+		System.out.print(question + " ");
+		String input = scanner.nextLine();
+		long value = Long.parseLong(input);
+		return value;
+	}
+
+	public static short requestShort(String question) {
+		System.out.print(question + " ");
+		String input = scanner.nextLine();
+		short value = Short.parseShort(input);
+		return value;
+	}
+
+	public static String requestString(String question) {
+		System.out.print(question + " ");
+		String input = scanner.nextLine();
+		return input;
 	}
 }
